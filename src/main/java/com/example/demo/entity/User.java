059d5b3d238a4ce925e30fdde66a0a17a03787db
Aymen.Chaari <aymen.chaari@kbmconsulting.fr>
2018-09-26 18:47:33 +0200

    utilisateur

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,90 @@
+package com.example.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+ private Long id;
+	@Column(unique=true)
+ private String username;
+ private String password;
+ private boolean enable;
+ 
+public User() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+
+public User( String username, String password, boolean enable) {
+	super();
+	
+	this.username = username;
+	this.password = password;
+	this.enable = enable;
+}
+
+public Long getId() {
+	return id;
+}
+
+public void setId(Long id) {
+	this.id = id;
+}
+
+public String getUsername() {
+	return username;
+}
+
+public void setUsername(String username) {
+	this.username = username;
+}
+
+public String getPassword() {
+	return password;
+}
+
+public void setPassword(String password) {
+	this.password = password;
+}
+
+public boolean isEnable() {
+	return enable;
+}
+
+public void setEnable(boolean enable) {
+	this.enable = enable;
+}
+
+@Override
+public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((id == null) ? 0 : id.hashCode());
+	return result;
+}
+
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	User other = (User) obj;
+	if (id == null) {
+		if (other.id != null)
+			return false;
+	} else if (!id.equals(other.id))
+		return false;
+	return true;
+}
+
+ 
+}
