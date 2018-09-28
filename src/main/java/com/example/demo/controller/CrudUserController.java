6c8dfe2a688427162c03146aad066f6609c2c334
Aymen.Chaari <aymen.chaari@kbmconsulting.fr>
2018-09-28 18:20:18 +0200

    1/2 tuto 24

--- src/main/java/com/example/demo/controller/CrudUserController.java
+++ src/main/java/com/example/demo/controller/CrudUserController.java
@@ -16,7 +16,7 @@ import com.example.demo.service.ICrudService;
 
 
 @RestController
-@RequestMapping(name="/crud_user")
+@RequestMapping("/crud_user")
 public class CrudUserController {
 	
 	@Autowired
